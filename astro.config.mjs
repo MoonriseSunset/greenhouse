7ec672620f6fad8e122b70ba23fc296368a743b6
Moonrise (Desktop) <aliuesch@gmail.com>
2025-01-07 00:54:45 -0700

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,37 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://moonrisesunset.github.io',
+  integrations: [	
+    starlight({
+      title: "WIP",
+			customCss:[
+				'./src/styles/starlight.css',
+				'@fontsource-variable/inter',
+				'@fontsource-variable/sometype-mono',
+				'@fontsource-variable/noto-sans',
+				'@fontsource/atkinson-hyperlegible',
+			],
+			social: {
+				github: 'https://github.com/MoonriseSunset',
+			},
+			favicon: './assets/starlightfavicon.png',
+      		logo: {src:'./src/assets/Logo.png',},
+			sidebar: [
+				{
+					label: 'Start Here',
+					items:[
+						{label: 'Landing Page',link: '/'},
+						{label: 'Changelog',link: '/changelog/'},
+					
+					],
+					collapsed: false,
+				},
+			],
+		}),
+  ],
+});
